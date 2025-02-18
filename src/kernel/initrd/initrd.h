919524fc467982e306d96ff833f4699846769dd2
Etienne Maire <etiennemaire629@gmail.com>
2025-02-18 14:18:18 +0100

    Added better initrd handling

--- src/kernel/initrd/initrd.h
+++ src/kernel/initrd/initrd.h
@@ -1,5 +1,19 @@
 #pragma once
 
+struct initrd_file
+{
+    char* name;
+    uint32_t size;
+    uint8_t* data;
+
+    // 12 bytes
+};
+
+#define MAX_INITRD_FILES 32
+
+struct initrd_file initrd_files[MAX_INITRD_FILES];
+uint8_t initrd_files_count = 0;
+
 void initrd_parse()
 {
     LOG(INFO, "Parsing initrd");
@@ -33,8 +47,13 @@ void initrd_parse()
 
         LOG(INFO, "    File : %s ; Size : %u bytes", header->name, file_size);
 
+        initrd_files[initrd_files_count].name = &header->name[0];
+        initrd_files[initrd_files_count].size = file_size;
+        initrd_files[initrd_files_count].data = (uint8_t*)(header + 1); // 512 bytes after the header
+        initrd_files_count++;
+
         initrd_offset += (file_size + USTAR_BLOCK_SIZE - 1) / USTAR_BLOCK_SIZE * USTAR_BLOCK_SIZE + USTAR_BLOCK_SIZE;
     }
 
-    LOG(INFO, "Done parsing initrd");
+    LOG(INFO, "Done parsing initrd (%u files)", initrd_files_count);
 }
