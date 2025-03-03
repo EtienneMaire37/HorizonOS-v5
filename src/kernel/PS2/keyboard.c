6857c6e14b4799b50c499ce4dd8d79a48f13de37
Etienne Maire <layzy37@gmail.com>
2025-03-03 21:39:12 +0100

    Fixed the scancode set not being read properly

--- src/kernel/PS2/keyboard.c
+++ src/kernel/PS2/keyboard.c
@@ -5,9 +5,12 @@ void ps2_kb_get_scancode_set()
     if (PS2_DEVICE_1_KB)
     {
         ps2_send_device_command(1, PS2_KB_GET_SET_SCANCODE_SET);
+        ps2_flush_buffer();
         ps2_send_device_command(1, 0);  // GET
         ps2_read_data();
         ps2_kb_1_scancode_set = ps2_data_buffer[0] == PS2_ACK ? ps2_data_buffer[1] : 0xff;
+
+        ps2_print_read_buffer();
     }
     else
         ps2_kb_1_scancode_set = 0xff;
@@ -15,6 +18,7 @@ void ps2_kb_get_scancode_set()
     if (PS2_DEVICE_2_KB)
     {
         ps2_send_device_command(2, PS2_KB_GET_SET_SCANCODE_SET);
+        ps2_flush_buffer();
         ps2_send_device_command(2, 0);  // GET
         ps2_read_data();
         ps2_kb_2_scancode_set = ps2_data_buffer[0] == PS2_ACK ? ps2_data_buffer[1] : 0xff;
@@ -25,6 +29,8 @@ void ps2_kb_get_scancode_set()
 
 void ps2_init_keyboards()
 {
+    ps2_flush_buffer();
+
     ps2_kb_get_scancode_set();
 
     if (PS2_DEVICE_1_KB)
