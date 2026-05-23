a8709ff7fe3731fb6a555a908edf10494057d81a
EtienneMaire37 <etiennemaire629@gmail.com>
2026-05-23 21:07:36 +0200

    fix: fix quotes in assert fail

--- src/kernel/libc/assert.c
+++ src/kernel/libc/assert.c
@@ -10,12 +10,12 @@ void __assert_fail(const char* assertion, const char* file, unsigned int line, c
     pid_t pid = multitasking_enabled ? current_task->pid : -1;
     if (function && function[0])
     {
-        printf("[pid = %d] %s:%u: %s%sAssertion `%s' failed.\n",
+        printf("[pid = %d] %s:%u: %s%sAssertion `%s` failed.\n",
                 pid,
                 file, line,
                 function, function[0] ? ": " : "",
                 assertion);
-        LOG(CRITICAL, "[pid = %d] %s:%u: %s%sAssertion `%s' failed.",
+        LOG(CRITICAL, "[pid = %d] %s:%u: %s%sAssertion `%s` failed.",
                 pid,
                 file, line,
                 function, function[0] ? ": " : "",
@@ -23,11 +23,11 @@ void __assert_fail(const char* assertion, const char* file, unsigned int line, c
     }
     else
     {
-        printf("[pid = %d] %s:%u: Assertion `%s' failed.\n",
+        printf("[pid = %d] %s:%u: Assertion `%s` failed.\n",
                 pid,
                 file, line,
                 assertion);
-        LOG(CRITICAL, "[pid = %d] %s:%u: Assertion `%s' failed.",
+        LOG(CRITICAL, "[pid = %d] %s:%u: Assertion `%s` failed.",
                 pid,
                 file, line,
                 assertion);
